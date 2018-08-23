fcb95fa9c9fb235797e34e5a28f269babe601a42
Kevin Fafard <1641392@cmontmorency.qc.ca>
2018-08-23 15:21:05 -0400

    Ajout de la premiere ligne Log

--- app/src/main/java/ca/cours5b5/kevinfafard/Parametres.java
+++ app/src/main/java/ca/cours5b5/kevinfafard/Parametres.java
@@ -2,12 +2,14 @@ package ca.cours5b5.kevinfafard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Parametres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MonEtiquette","Bonjour!");
         setContentView(R.layout.activity_parametres);
     }
 }
